9d022b5b4f79a59cc56ea03682a69ce5ec33292b
Ajith <ajith@localhost.localdomain>
2022-08-15 22:35:33 +0530

    js adding

---
+++ script.js
@@ -0,0 +1,21 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+var scrolled = (winScroll / height) * 100;
+document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+fetch('https://api.github.com/user/ajithshine')
+.then(res=>{
+    // console.log(res.json)
+    return res.json()
+})
+.then(data=>{
+    console.log(data)
+    document.querySelector("#followers").innerText=data.followers
+    document.querySelector("#following").innerText=data.following
+    document.querySelector("#repos").innerText=data.public_repos
+    document.querySelector("#gists").innerText=data.public_gists
+})
